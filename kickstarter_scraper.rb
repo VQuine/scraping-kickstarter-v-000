5e862283fe293251db139255431a0bb537516873
Kimberly Aviles <01010110.v@gmail.com>
2019-03-27 04:49:33 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,15 +3,15 @@ require 'nokogiri'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
+  kickstarter = Nokogiri::HTML(html) ### Nokogiri inherits all methods from Ruby's HTML module, I think? and passes in the site to scrape
 
-  projects = {}
+  projects = {} ### create a place to store every project scraped
 
-  kickstarter.css("li.project.grid_4").each do |project|
+  kickstarter.css("li.project.grid_4").each do |project| ### iterate through each individual project section
 
-    title = project.css("h2.bbcard_name strong a").text
+    title = project.css("h2.bbcard_name strong a").text ### scrapes the project title and stores it
 
-    projects[title.to_sym] = {
+    projects[title.to_sym] = { ### turns the title string into a symbol and puts it in the PROJECTS hash - the value becomes all the scraped data about the project
 
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
@@ -22,6 +22,6 @@ def create_project_hash
 
   end
 
-  projects
+  projects ### implicit return of the filled up PROJECTS hash
 
 end
